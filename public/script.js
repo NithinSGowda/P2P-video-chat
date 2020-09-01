cca24664f47b115963bd8eb5e9d61295be0c9ff7
root <root@localhost.localdomain>
2020-09-01 14:15:34 +0530

    n

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
-  port: '3001'
+  port: '443'
 })
 let myVideoStream;
 const myVideo = document.createElement('video')
